6df0a79359098429f7cc92ddb4e922fe6fa5e9ac
Romain Parage <rparage@talentsoft.com>
2021-07-28 22:06:04 +0200

    Add node relations

--- src/Repository/Entities/Node.cs
+++ src/Repository/Entities/Node.cs
@@ -1,20 +1,28 @@
 ﻿namespace Repository.Entities
 {
     using System;
+    using System.Collections.Generic;
     using Repository.Entities.Base;
 
     public class Node : EntityBase
     {
-        public Tree Tree { get; set; } = null!;
+        public Tree? Tree { get; set; }
 
         public Guid TreeId { get; set; }
 
         public string Label { get; set; }
 
+        public Node? Parent { get; set; }
+
+        public Guid? ParentId { get; set; }
+
+        public List<Node> Children { get; set; }
+
         public Node(Guid treeId, string label)
         {
             TreeId = treeId;
             Label = label;
+            Children = new List<Node>();
         }
     }
 }
--- src/Repository/Migrations/20210728195922_AddNodeRelations.Designer.cs
+++ src/Repository/Migrations/20210728195922_AddNodeRelations.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repository.Contexts;
 
 namespace Repository.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20210728195922_AddNodeRelations")]
+    partial class AddNodeRelations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
