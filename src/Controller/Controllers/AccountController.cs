a54eb518f89af9d1fa562b0b29185657543cf418
Romain Parage <rparage@talentsoft.com>
2022-01-20 22:55:16 +0100

    Add code to create user and tenant at login

--- src/Controller/Controllers/AccountController.cs
+++ src/Controller/Controllers/AccountController.cs
@@ -7,24 +7,66 @@
     using Microsoft.AspNetCore.Authentication.Cookies;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Repository.Contexts;
+    using Repository.Entities;
 
     [Route("account")]
     [AllowAnonymous]
     public class AccountController : CustomControllerBase
     {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public AccountController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
         [Route("login")]
         public async Task<IActionResult> Login()
         {
-            // Todo - Handle user creation properly
+            (Guid userId, Guid tenantId) = await GetOrCreateDefaultUserAndTenant();
             var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
-            identity.AddClaim(new Claim(ApplicationClaims.TenantId, "db70957e-4faa-4169-80be-f5d543c98cc2"));
-            identity.AddClaim(new Claim(ApplicationClaims.TenantId, "1"));
+            identity.AddClaim(new Claim(ApplicationClaims.TenantId, tenantId.ToString()));
+            identity.AddClaim(new Claim(ApplicationClaims.UserId, userId.ToString()));
             var principal = new ClaimsPrincipal(identity);
             var authProperties = new AuthenticationProperties();
 
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(principal), authProperties);
 
             return Redirect("/");
+        }
+
+        // Todo - Temporary code to simplify user creation
+        private async Task<(Guid UserId, Guid TenantId)> GetOrCreateDefaultUserAndTenant()
+        {
+            const string defaultTenantName = "Default Tenant";
+            const string defaultUserName = "Default Name";
+
+            Tenant? tenant = await _applicationDbContext.Tenants.SingleOrDefaultAsync(p => p.Name == defaultTenantName);
+
+            if (tenant == null)
+            {
+                tenant = new Tenant(defaultTenantName);
+                _applicationDbContext.Tenants.Add(tenant);
+            }
+
+            User? user = await _applicationDbContext.Users.Include(p => p.Tenants).SingleOrDefaultAsync(p => p.UserName == defaultUserName);
+
+            if (user == null)
+            {
+                user = new User { UserName = defaultUserName };
+                _applicationDbContext.Users.Add(user);
+            }
+
+            if (user.Tenants.All(p => p.Id != tenant.Id))
+            {
+                user.Tenants.Add(tenant);
+            }
+
+            await _applicationDbContext.SaveChangesAsync();
+
+            return (tenant.Id, user.Id);
         }
     }
 }
